6128a447f96fe8792cbd0746825913b77d0d19d3
YDobryansky <168298265+YDobryansky@users.noreply.github.com>
2024-05-19 23:31:48 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,4 +1,26 @@
-export const getSquaredArray = arr => arr.map(num => num * num);
 
-export const getOddNumbers = arr => arr.filter(num => num % 2 === 1);
-export default (a, b) => a + b;
+export const calc = expression => {
+  if (typeof expression !== 'string') {
+    return null;
+  }
+
+  const [a, operation, b] = expression.split(' ');
+  let result;
+
+  switch (operation) {
+    case '+':
+      result = +a + +b;
+      break;
+    case '-':
+      result = +a - +b;
+      break;
+    case '*':
+      result = +a * +b;
+      break;
+    case '/':
+      result = +a / +b;
+      break;
+  }
+
+  return `${expression} = ${result}`;
+};
